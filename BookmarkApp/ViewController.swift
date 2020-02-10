e7ce5f011cf429e8a82ecbd35d87b4b2a277d562
main-endp <endp@kakao.com>
2020-02-10 18:08:52 +0900

    initial commit

---
+++ BookmarkApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BookmarkApp
+//
+//  Created by 장창순 on 10/02/2020.
+//  Copyright © 2020 AnAppPerTwoWeeks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
