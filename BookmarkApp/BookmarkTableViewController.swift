97ad0819ae17250c9bfb2b82a3fbc26d57411ff3
mainendp <endp@kakao.com>
2020-02-13 00:32:31 +0900

    북마크 정보를 입력할 수 있는 팝업을 표시한다. #3

--- BookmarkApp/BookmarkTableViewController.swift
+++ BookmarkApp/BookmarkTableViewController.swift
@@ -29,7 +29,7 @@ class BookmarkTableViewController: UITableViewController {
         return bookmarkArray.count
     }
 
-    
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! BookmarkTableViewCell
         cell.bookmarkLabel.text = bookmarkArray[indexPath.row]
@@ -43,51 +43,29 @@ class BookmarkTableViewController: UITableViewController {
 
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         print("북마크 추가 버튼이 클릭 되었다.")
+        
+        let alert = UIAlertController(title: "북마크 추가", message: nil, preferredStyle: .alert)
+        
+        let cancelAction = UIAlertAction(title: "취소", style: .destructive) { (action) in
+            print("취소 버튼이 클릭 되었다.")
+        }
+        
+        let addAction = UIAlertAction(title: "저장", style: .default) { (action) in
+            print("저장 버튼이 클릭 되었다.")
+        }
+        
+        alert.addTextField { (alertNameTextfield) in
+            alertNameTextfield.placeholder = "북마크 이름을 입력해 주세요."
+        }
+        
+        alert.addTextField { (alertURLTextfield) in
+            alertURLTextfield.placeholder = "URL을 입력해 주세요."
+        }
+        
+        alert.addAction(cancelAction)
+        alert.addAction(addAction)
+        
+        present(alert, animated: true)
+        
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
