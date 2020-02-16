3fc49faf567312be9ed8f6a33fd02fdbee37d8f6
mainendp <endp@kakao.com>
2020-02-17 01:18:27 +0900

    저장된 북마크를 편집한다 #9

--- BookmarkApp/EditBookmarkViewController.swift
+++ BookmarkApp/EditBookmarkViewController.swift
@@ -13,8 +13,17 @@ class EditBookmarkViewController: UIViewController {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var urlLabel: UILabel!
     @IBOutlet weak var editButton: UIButton!
+    @IBOutlet weak var nameTextfield: UITextField!
+    @IBOutlet weak var urlTextfield: UITextField!
     
-    var bookmarkArr = [BookmarkVO]()
+    let defaults = UserDefaults.standard
+    
+    var bookmarkName = ""
+    var bookmarkURL = ""
+    var indexpath = 0
+    
+    var bookmark = BookmarkVO()
+    var EditedBookmarkArray = [BookmarkVO]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,11 +33,21 @@ class EditBookmarkViewController: UIViewController {
         urlLabel.text = "URL"
         editButton.setTitle("확인", for: .normal)
         
+        nameTextfield.text = bookmark.name
+        urlTextfield.text = bookmark.url
+
     }
     
-
-        
+    @IBAction func okButtonPressed(_ sender: Any) {
+        bookmark.name = nameTextfield.text
+        bookmark.url = urlTextfield.text
+        EditedBookmarkArray[indexpath] = bookmark
+        if let encode = try? JSONEncoder().encode(self.EditedBookmarkArray) {
+            self.defaults.set(encode, forKey: "bookmark")
+        }
+        self.navigationController?.popViewController(animated: true)
     }
+}
 
 
 
